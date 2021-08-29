b8a1063960e60138948b3e68551deb3bbf433d67
gaaronah <gaaronah@gmail.com>
2021-08-28 22:53:11 -0700

    Aether2 Color select

--- AchromaNeoOnline/Assets/Scripts/CharacterCards/AetherCard.cs
+++ AchromaNeoOnline/Assets/Scripts/CharacterCards/AetherCard.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class AetherCard : CharacterCard
 {
+    public Sprite[] aether2Sprites = new Sprite[4];
+    public char[] aether2Colors = new char[4];
+
     public override void LevelUp()
     {
-        if (color != 'C')
+        base.LevelUp();
+        if (level == 3)
         {
             color = 'C';
+        } else if (level == 2)
+        {
+            // Insert choose color phase
         }
-        base.LevelUp();
     }
 
     public override void ActivateSkill()
     {
         base.ActivateSkill();
+    }
+
+    public void ChooseColor(int i)
+    {
+        characterImage.sprite = aether2Sprites[i];
+        color = aether2Colors[i];
     }
 }
